80fa43c4f2590dc186e229382ea36b66f8d14338
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2021-02-08 12:23:33 +0530

    to test commit in branch and streamlit

---
+++ Streamlit_Work/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd 
+from matplotlib import pyplot as plt
+from plotly import graph_objs as go
+from sklearn.linear_model import LinearRegression
+import numpy as np 
+
+st.title("FOOOD Predictor")
+st.text("just to try out")
