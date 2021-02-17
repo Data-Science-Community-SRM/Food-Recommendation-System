4ec5aa14dae433b62133c8512d1f0982e57580dd
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2021-02-17 15:35:31 +0530

    Discussion after first meeting

--- Streamlit_Work/app.py
+++ Streamlit_Work/app.py
@@ -7,3 +7,26 @@ import numpy as np
 
 st.title("FOOOD Predictor")
 st.text("just to try out")
+st.image("foood.jpg")
+
+nav = st.sidebar.radio("Navigation",["Home","IF Necessary 1","If Necessary 2"])
+
+st.subheader("What Cuisine do you prefer?")
+cuisine = st.selectbox("Choose your favourite!",["Indian","Chinese","Continental","Middle Eastern",
+"Japenese"])
+
+st.subheader("Whats your preference?")
+vegn = st.radio("Vegetables or none!",["veg","non-veg"],index = 1)
+
+#bruh = st.checkbox("you accept the T&C") gives boolean value
+
+st.subheader("How much do you like the dish?")  #RATING
+val = st.slider("from poor to the best!",0,10)
+
+#veg nonver
+#cuisine
+#rating
+#display the food name data accordingly
+#dark mode
+
+#st.write(foods)
