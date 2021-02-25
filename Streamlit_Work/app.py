fd80f907571b052196c3d9a59cc1616508087480
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2021-02-25 10:18:13 +0530

    project completed before deployment

--- Streamlit_Work/app.py
+++ Streamlit_Work/app.py
@@ -4,9 +4,11 @@ from matplotlib import pyplot as plt
 from plotly import graph_objs as go
 from sklearn.linear_model import LinearRegression
 import numpy as np 
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
 
-st.title("FOOOD Predictor")
-st.text("just to try out")
+st.title("Food Recommendation System")
+st.text("Let us help you with ordering")
 st.image("foood.jpg")
 
 ## nav = st.sidebar.radio("Navigation",["Home","IF Necessary 1","If Necessary 2"])
@@ -32,9 +34,47 @@ names = ans['Name'].tolist()
 x = np.array(names)
 ans1 = np.unique(x)
 
-bruh = st.checkbox("Do you want to see our selection?")
+finallist = ""
+bruh = st.checkbox("Choose your Dish")
 if bruh == True:
     finallist = st.selectbox("Our Choices",ans1)
 
 
+##### IMPLEMENTING RECOMMENDER ######
+dataset = ratings.pivot_table(index='Food_ID',columns='User_ID',values='Review')
+dataset.fillna(0,inplace=True)
+csr_dataset = csr_matrix(dataset.values)
+dataset.reset_index(inplace=True)
 
+model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+model.fit(csr_dataset)
+
+def food_recommendation(Food_Name):
+    n = 10
+    FoodList = food[food['Name'].str.contains(Food_Name)]  
+    if len(FoodList):        
+        Foodi= FoodList.iloc[0]['Food_ID']
+        Foodi = dataset[dataset['Food_ID'] == Foodi].index[0]
+        distances , indices = model.kneighbors(csr_dataset[Foodi],n_neighbors=n+1)    
+        Food_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
+        Recommendations = []
+        for val in Food_indices:
+            Foodi = dataset.iloc[val[0]]['Food_ID']
+            i = food[food['Food_ID'] == Foodi].index
+            Recommendations.append({'Name':food.iloc[i]['Name'].values[0],'Distance':val[1]})
+        df = pd.DataFrame(Recommendations,index=range(1,n+1))
+        return df['Name']
+    else:
+        return "No Similar Foods."
+
+
+display = food_recommendation(finallist)
+#names1 = display['Name'].tolist()
+
+#x1 = np.array(names)
+#ans2 = np.unique(x1)
+if bruh == True:
+    bruh1 = st.checkbox("Our Recommendation : ")
+    if bruh1 == True:
+        for i in display:
+            st.write(i)
